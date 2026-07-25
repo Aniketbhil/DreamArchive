ed696a9314b4276fa4ab54824d3f1f76044222d5
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-07-25 20:22:21 +0530

    feat: integrate FPS player controller into Dream 1 environment

--- src/scenes/Dream1Scene.tsx
+++ src/scenes/Dream1Scene.tsx
@@ -1,22 +1,30 @@
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useState, useEffect } from "react";
+import * as THREE from "three";
 
 import FadeOverlay from "../components/transitions/FadeOverlay";
 import Dream1Models from "../world/dream1/Dream1Models";
 import PostProcessing from "../components/effects/PostProcessing";
+import FirstPersonCamera from "../components/player/FirstPersonCamera";
 
 export default function Dream1Scene() {
+  const [started, setStarted] = useState(false);
+
+  // Wait 800ms for the black screen to start fading before allowing movement
+  useEffect(() => {
+    const timer = setTimeout(() => setStarted(true), 800);
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Adjust these coordinates to place the player perfectly on your island
+  const spawn = new THREE.Vector3(0, 5, 20);
+  const target = new THREE.Vector3(0, 5, 0); // Look towards the center
+
   return (
     <>
-      <Canvas
-        shadows
-        dpr={[1, 1.5]}
-        // We set the camera a bit further back initially so you can see the whole island
-        camera={{ position: [0, 20, 60], fov: 45, near: 0.1, far: 3000 }}
-      >
+      <Canvas shadows dpr={[1, 1.5]} camera={{ fov: 45, near: 0.1, far: 3000 }}>
         <color attach="background" args={["#020204"]} />
 
-        {/* Basic Dream 1 Lighting Setup */}
         <ambientLight intensity={0.4} />
         <hemisphereLight
           intensity={0.6}
@@ -32,6 +40,9 @@ export default function Dream1Scene() {
 
         <Suspense fallback={null}>
           <Dream1Models />
+
+          <FirstPersonCamera spawn={spawn} target={target} started={started} />
+
           <PostProcessing />
         </Suspense>
       </Canvas>
